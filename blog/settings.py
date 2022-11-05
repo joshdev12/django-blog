5fea8575a49e75dbf349b834ad8bd27de5585cc5
joshdev <joshuaajayi012@gmail.com>
2022-11-05 06:08:44 +0100

    update set

--- blog/settings.py
+++ blog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["web-production-80bc.up.railway.app"]
+ALLOWED_HOSTS = ["silvakidd.up.railway.app"]
 
 
 # Application definition
